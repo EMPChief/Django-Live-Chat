7a7054cd88e160995ebca7ae64785851743e8b0d
empchief <bjornmagnekristensen1999@gmail.com>
2024-01-13 19:22:19 +0100

    Add UserProfile model registration functionality login functionality logout functionality

--- account/views.py
+++ account/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, authenticate, login
+from django.contrib import messages
+from .models import UserProfile
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            userprofile = UserProfile(user=user)
+            userprofile.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+        for field in form.fields:
+            form.fields[field].widget.attrs.update({
+                'class': 'shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            })
+
+    context = {'form': form}
+    return render(request, 'account/register.html', context)
+def custom_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Invalid username or password.')
+
+    return render(request, 'account/login.html')
+
+
+def custom_logout(request):
+    logout(request)
+    return redirect('index')
